c03f7b141e0e2e7fcd056557da1f5f2f2ad6340a
Milan Burmaja <burmajam@gmail.com>
2017-02-09 15:26:10 +0100

    Created GenAggregate

---
+++ lib/system/gen_aggregate.ex
@@ -0,0 +1,137 @@
+defmodule Extreme.System.GenAggregate do
+  @moduledoc """
+  Generic Aggregate module.
+
+  ## Example
+
+      defmodule MyAggregate do
+        use Extreme.System.GenAggregate
+      
+        defmodule State, do: defstruct [:transaction, :ttl, :events, :buffer, :msg]
+      
+        ## Client API
+      
+        def start_link(ttl \\ 2_000), do: GenAggregate.start_link __MODULE__, ttl
+      
+        def do_something(pid, val), do: exec pid, {:do_something, val}
+      
+        def message(pid), do: exec pid, :get_message
+      
+        ## Server Callbacks
+      
+        def init(ttl), do: {:ok, %State{buffer: [], msg: "", ttl: ttl}} 
+
+        def handle_exec({:do_something, val}, from, state) do
+          events = [%{val: val}]
+          result = {:ok, state.transaction, events}
+      
+          {:block, from, result, %{state | events: events}}
+        end
+        def handle_exec(:get_message, from, state) do
+          {:noblock, from, state.msg, state}
+        end
+      
+        defp apply_events([%{val: val} | tail], state) do
+          state = %{ state | msg: state.msg <> val }
+          apply_events tail, state
+        end
+      end
+
+    Use it then as:
+    {:ok, a} = MyAggregate.start_link
+    {:ok, transaction_id, _} = MyAggregate.do_something(a, "something")
+    :ok = MyAggregate.commit a, transaction_id
+    MyAggregate.message(a)
+    #=> "something"
+  """
+
+  def start_link(module, init_values, options \\ []) do 
+    GenServer.start_link module, init_values, options
+  end
+
+  defmacro __using__(_) do
+    quote do
+      use GenServer
+      alias Extreme.System.GenAggregate
+      #require Logger
+
+      def commit(pid, transaction), do: GenServer.call(pid, {:commit, transaction})
+
+      def exec(pid, cmd), do: GenServer.call(pid, {:cmd, cmd})
+
+      def reply(to, payload), do: GenServer.reply to, payload
+
+      def apply(pid, events), do: GenServer.call(pid, {:apply_stream_events, events})
+
+      def handle_call({:cmd, cmd}, from, %{buffer: [], transaction: nil}=state) do
+        #Logger.debug "Executing: #{inspect cmd}"
+        lock = make_ref
+        GenServer.cast self, {:execute, {cmd, from}}
+        {:noreply, %{state | transaction: lock}}
+      end
+      def handle_call({:cmd, cmd}, from, %{}=state) do
+        #Logger.debug "Buffering: #{inspect cmd}"
+        buffer = [{cmd, from} | state.buffer]
+        {:noreply, %{state | buffer: buffer}}
+      end
+      def handle_call({:commit, nil}, _from, state) do 
+      {:reply, {:error, :nil_transaction}, state}
+      end
+      def handle_call({:commit, transaction}, _from, %{transaction: transaction}=state) do
+        #Logger.debug "Commiting: #{inspect transaction}"
+        state = apply_events state.events, state
+        GenServer.cast self, :process_buffer
+        {:reply, :ok, %{state | transaction: nil, events: []}}
+      end
+      def handle_call({:commit, t1}, _from, %{transaction: transaction}=state) do 
+        {:reply, {:error, :wrong_transaction}, state}
+      end
+      def handle_call({:apply_stream_events, events_stream}, _from, state) do
+        state = Enum.reduce(events_stream, state, fn({event, _event_number}, acc) -> apply_events([event], acc) end)
+        {:reply, :ok, state}
+      end
+
+
+      def handle_cast(:process_buffer, %{buffer: []}=state), do: {:noreply, state}
+      def handle_cast(:process_buffer, %{buffer: buffer, transaction: nil}=state) do
+        lock = make_ref
+        {cmd, from} = List.last buffer
+        #Logger.debug "Processing buffered cmd: #{inspect cmd}"
+        buffer = List.delete_at buffer, -1
+        GenServer.cast self, {:execute, {cmd, from}}
+        {:noreply, %{state | buffer: buffer, transaction: lock}}
+      end
+      def handle_cast(:process_buffer, state), do: {:noreply, state}
+      def handle_cast({:execute, {cmd, from}}, state) do
+        case handle_exec(cmd, from, state) do
+          {:block, from, {:events, events}, state} when is_list(events) ->
+            schedule_rollback state.transaction, state.ttl
+            GenServer.reply from, {:ok, state.transaction, events}
+            {:noreply, %{state | events: events}}
+          {:block, from, response, state} ->
+            schedule_rollback state.transaction, state.ttl
+            GenServer.reply from, response
+            {:noreply, state}
+          {:noblock, from, response, state} ->
+            GenServer.reply from, response
+            {:noreply, %{state | transaction: nil}}
+        end
+      end
+
+      def handle_info({:rollback, transaction}, %{transaction: transaction}=state) do 
+      #Logger.warn "Rolling back transaction #{inspect transaction}"
+      GenServer.cast self, :process_buffer
+      {:noreply, %{state | transaction: nil}}
+      end
+      def handle_info(_, state), do: {:noreply, state}
+
+      defp schedule_rollback(transaction, ttl) do 
+        {:ok, _ref} = :timer.send_after ttl, self, {:rollback, transaction}
+      end
+
+      defp apply_events([], state), do: state
+
+      defoverridable [handle_info: 2]
+    end
+  end
+end
---
+++ test/system/gen_aggregate_test.exs
@@ -0,0 +1,100 @@
+defmodule Aggregate do
+  use     Extreme.System.GenAggregate
+  require Logger
+
+  defmodule State, do: defstruct [:transaction, :ttl, :events, :buffer, :msg]
+
+  ## Client API
+
+  def start_link(ttl \\ 2_000), do: GenAggregate.start_link __MODULE__, ttl
+
+  def do_something(pid, val) do 
+    Logger.debug "Try #{val}"
+    exec pid, {:do_something, val}
+  end
+
+  def message(pid), do: exec pid, :get_message
+
+
+  ## Server Callbacks
+
+  def init(ttl), do: {:ok, %State{events: [], buffer: [], msg: "", ttl: ttl}}
+
+  def handle_exec({:do_something, val}, from, state) do
+    Logger.debug "Doing #{val}"
+    events = [%{val: val}]
+    {:block, from, {:events, events}, state}
+  end
+  def handle_exec(:get_message, from, state) do
+    {:noblock, from, state.msg, state}
+  end
+
+  defp apply_events([%{val: val} | tail], state) do
+    state = %{ state | msg: state.msg <> val }
+    apply_events tail, state
+  end
+end
+
+defmodule Extreme.System.GenAggregateTest do
+  use     ExUnit.Case, async: true
+  doctest Extreme.System.GenAggregate
+
+  setup do
+    {:ok, a} = Aggregate.start_link
+    {:ok, %{a: a}}
+  end
+
+  test "can commit active transaction", %{a: a} do
+    {:ok, transaction_id, _} = Aggregate.do_something(a, "something")
+    :ok = Aggregate.commit a, transaction_id
+    assert Aggregate.message(a) == "something"
+  end
+
+  test "can't commit nil transaction", %{a: a} do
+    assert {:error, :nil_transaction} =  Aggregate.commit(a, nil)
+  end
+
+  test "can't commit wrong transaction", %{a: a} do
+    {:ok, _, _} = Aggregate.do_something(a, "something")
+    assert {:error, :wrong_transaction} =  Aggregate.commit(a, :wrong_transaction)
+  end
+
+  test "waits with second command until first is commited", %{a: a} do
+    for n <- 1..5 do
+      :timer.sleep 10
+      spawn fn ->
+        {:ok, transaction_id_2, _} = Aggregate.do_something(a, "#{n} ")
+        :timer.sleep n * 100
+        :ok = Aggregate.commit a, transaction_id_2
+      end
+    end
+    :timer.sleep 10
+    {:ok, transaction_id_1, _} = Aggregate.do_something(a, "else ")
+    for n <- 6..9 do
+      :timer.sleep 10
+      spawn fn ->
+        {:ok, transaction_id_2, _} = Aggregate.do_something(a, "#{n} ")
+        :ok = Aggregate.commit a, transaction_id_2
+      end
+    end
+    :ok = Aggregate.commit a, transaction_id_1 
+    assert Aggregate.message(a) == "1 2 3 4 5 else 6 7 8 9 "
+  end
+
+  test "timeout", %{a: a} do
+    {:ok, transaction_id_1, _} = Aggregate.do_something(a, "1 ")
+    :ok = Aggregate.commit a, transaction_id_1
+    assert Aggregate.message(a) == "1 "
+    {:ok, transaction_id_3, _} = Aggregate.do_something(a, "throw_away")
+    for n <- 2..5 do
+      :timer.sleep 10
+      spawn fn ->
+        {:ok, transaction_id_2, _} = Aggregate.do_something(a, "#{n} ")
+        :ok = Aggregate.commit a, transaction_id_2
+      end
+    end
+    :timer.sleep 3_000
+    {:error, :wrong_transaction} = Aggregate.commit a, transaction_id_3
+    assert Aggregate.message(a) == "1 2 3 4 5 "
+  end
+end
