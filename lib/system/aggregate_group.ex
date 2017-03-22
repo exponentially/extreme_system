8503b6e91ba8929ade530dc3a9295dc4b72f96f5
Milan Burmaja <burmajam@gmail.com>
2017-03-22 10:27:40 +0100

    squeeze this

--- lib/system/aggregate_group.ex
+++ lib/system/aggregate_group.ex
@@ -3,30 +3,29 @@ defmodule Extreme.System.AggregateGroup do
   alias   Extreme.System.AggregateSup
   alias   Extreme.System.PidRegistry
 
-  def start_link(group_name, aggregate_module) do
-    name = "#{group_name}.AggregateGroup" |> String.to_atom
-    Supervisor.start_link __MODULE__, {group_name, aggregate_module}, name: name
-  end
+  def start_link(group, aggregate_module),
+    do: Supervisor.start_link __MODULE__, {group, aggregate_module}, name: group_name(group)
 
-  def spawn_new(group_name, key, child_params \\ []) do
-    {:ok, pid} = group_name |> sup_name      |> AggregateSup.start_child(child_params)
-    :ok        = group_name |> registry_name |> PidRegistry.register(key, pid)
+  def spawn_new(group, key, child_params \\ []) do
+    {:ok, pid} = group |> sup_name      |> AggregateSup.start_child(child_params)
+    :ok        = group |> registry_name |> PidRegistry.register(key, pid)
     {:ok, pid}
   end
 
-  def get_registered(group_name, key),
-    do: group_name |> registry_name |> PidRegistry.get(key)
+  def get_registered(group, key),
+    do: group |> registry_name |> PidRegistry.get(key)
 
 
-  def init({group_name, aggregate_module}) do
+  def init({group, aggregate_module}) do
     children = [
-      supervisor( AggregateSup, [aggregate_module, [name: sup_name(group_name)]]),
-      worker(     PidRegistry,  [[name: registry_name(group_name)]])
+      supervisor( AggregateSup, [aggregate_module, [name: sup_name(group)]]),
+      worker(     PidRegistry,  [[name: registry_name(group)]])
     ]
     supervise children, strategy: :one_for_all
   end
 
 
-  defp sup_name(group_name),      do: "#{group_name}.AggregateSup"      |> String.to_atom
-  defp registry_name(group_name), do: "#{group_name}.AggregateRegistry" |> String.to_atom
+  defp group_name(group),    do: "#{group}.AggregateGroup"    |> String.to_atom
+  defp sup_name(group),      do: "#{group}.AggregateSup"      |> String.to_atom
+  defp registry_name(group), do: "#{group}.AggregateRegistry" |> String.to_atom
 end
