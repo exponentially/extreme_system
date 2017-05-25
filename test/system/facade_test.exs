621fdb9870454b0641951ceb1113ce1336ab24e1
Milan Burmaja <burmajam@gmail.com>
2017-05-25 13:34:10 +0200

    Facade proxies message to handler's action with different name

--- test/system/facade_test.exs
+++ test/system/facade_test.exs
@@ -13,7 +13,8 @@ defmodule MyFacade do
   def on_init,
     do: Logger.info "Started MyFacade"
 
-  route :cmd, MyMsgHandler
+  route :cmd,  MyMsgHandler
+  route :cmd2, {MyMsgHandler, :cmd}
 end
 
 defmodule Extreme.System.FacadeTest do
@@ -21,12 +22,16 @@ defmodule Extreme.System.FacadeTest do
   require Logger
 
   setup do
-    {:ok, _} = Task.Supervisor.start_link name: :request_sup
-    {:ok, _} = MyFacade.start_link :request_sup, name: MyFacade
-    :ok
+    {:ok, _}      = Task.Supervisor.start_link name: :request_sup
+    {:ok, facade} = MyFacade.start_link :request_sup
+    {:ok, %{facade: facade}}
   end
 
-  test "proxies message to handler's action with the same name" do
-    assert :ok = GenServer.call MyFacade, {:cmd, 123}
+  test "proxies message to handler's action with the same name", %{facade: facade} do
+    assert :ok = GenServer.call facade, {:cmd, 123}
+  end
+
+  test "proxies message to handler's action with different name", %{facade: facade} do
+    assert :ok = GenServer.call facade, {:cmd2, "different call"}
   end
 end
