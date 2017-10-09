497bca813ae418895a9e1387c8dcdf18809df986
Milan Burmaja <burmajam@gmail.com>
2017-10-09 12:51:49 +0200

    updated depenendecies

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Extreme.System.Mixfile do
 
   def project do
     [app: :extreme_system,
-     version: "0.2.0",
-     elixir: "~> 1.3 or ~> 1.4 or ~> 1.5",
+     version: "0.2.1",
+     elixir: "~> 1.3.0 or ~> 1.4.0 or ~> 1.5.0",
      source_url: "https://github.com/exponentially/extreme_system",
      description: """
      Building blocks for distributed systems
@@ -27,7 +27,7 @@ defmodule Extreme.System.Mixfile do
 
   defp deps do
     [
-      {:extreme, "~> 0.9.0"},
+      {:extreme, "~> 0.10.0"},
       {:cachex, "~> 2.1"},
       {:amqp,    "~> 0.1.4 or ~> 0.2.2"},
       {:ex_doc, ">= 0.11.4", only: [:dev]},
