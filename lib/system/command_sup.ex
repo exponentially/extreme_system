40fabdffb1277fef3de8ef84ecd44f1e2d071b47
Milan Burmaja <burmajam@gmail.com>
2017-11-10 13:45:53 +0100

    [CommandSup] Doesn't require extreme_settings if aggregates are not event sourced

--- lib/system/command_sup.ex
+++ lib/system/command_sup.ex
@@ -2,15 +2,19 @@ defmodule Extreme.System.CommandSup do
   use     Supervisor
   alias   Extreme.System, as: ExSys
 
-  def start_link(configuration_module, prefix, extreme_settings), 
+  def start_link(configuration_module, prefix, extreme_settings \\ nil), 
     do: Supervisor.start_link __MODULE__, {configuration_module, prefix, extreme_settings}, name: name(prefix)
 
   def init({configuration_module, prefix, extreme_settings}) do
-    children = [
-      supervisor( configuration_module, [prefix]),
-      worker(     Extreme,              [extreme_settings,                           [name: extreme_name(prefix)]]),
-      worker(     ExSys.EventStore,     [configuration_module, extreme_name(prefix), [name: ExSys.EventStore.name(prefix)]]),
-    ]
+    children = if extreme_settings do
+      [
+        worker(     Extreme,              [extreme_settings,                           [name: extreme_name(prefix)]]),
+        worker(     ExSys.EventStore,     [configuration_module, extreme_name(prefix), [name: ExSys.EventStore.name(prefix)]]),
+      ]
+    else
+      []
+    end
+    children = [ supervisor( configuration_module, [prefix]) | children ]
     supervise children, strategy: :one_for_one
   end
 
